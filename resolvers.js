7db61901170f42318b0d18853aca5f391ae1b5a1
ismailharoun995 <ismailharoun01@gmail.com>
2023-10-07 18:54:15 +0300

    first commit

---
+++ resolvers.js
@@ -0,0 +1,21 @@
+import { getJobs } from "./db/jobs.js";
+import { getCompany } from "./db/companies.js";
+
+export const resolvers = {
+    Query: {
+            job: (_root, {id}) => getJobs(id),
+        jobs: () => getJobs()
+        },
+
+        Job: {
+            company: (job)=>  getCompany(job.companyId),
+            date:(job) => toIsoDate(job.createdAt),
+        }
+
+    };
+    function toIsoDate(value){
+        return value.slice(0,'yyyy-mm-dd'.length);
+    }
+    
+
+
